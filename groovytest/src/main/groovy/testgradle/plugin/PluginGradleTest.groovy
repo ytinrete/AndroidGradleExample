e65908c1475331c8cc994ceae74aa6c64e8fc62a
lirui2015 <lirui2015@corp.netease.com>
2017-05-10 16:04:09 +0800

    insert task test

--- groovytest/src/main/groovy/testgradle/plugin/PluginGradleTest.groovy
+++ groovytest/src/main/groovy/testgradle/plugin/PluginGradleTest.groovy
@@ -10,6 +10,18 @@ public class PluginGradleTest implements Plugin<Project> {
     public void apply(Project project) {
         println( 'ya hooooo~~~~ it is the apply:' + project.name)
 
+        println('adding a task into it, and it can be seen outside.')
+
+        project.getTasks().create("a1FromPlugin", {
+
+            println("in the plugin insert task~")
+
+            doLast{
+                println("in the plugin insert task~  last")
+            }
+
+        })
+
         project.getTasks().each { task ->
             println( "kkk:" + task.name)
         }
